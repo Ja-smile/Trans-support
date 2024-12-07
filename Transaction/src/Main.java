e65f619285ad41ff9e7e144717031e096763f525
Tadania Tait <tadtait@gmail.com>
2024-12-06 19:52:27 -0500

    Remove lingering debugging statements

--- Transaction/src/Main.java
+++ Transaction/src/Main.java
@@ -6,10 +6,10 @@ public static void main(String[] args) {
     InMemoryDatabase inmemoryDB = new InMemoryDatabase();
 
     // should return null, because A doesn’t exist in the DB yet
-    System.out.println("1 " + inmemoryDB.get("A"));
+    System.out.println(inmemoryDB.get("A"));
 
     // should throw an error because a transaction is not in progress
-    inmemoryDB.put("A", 5); // comment out to test remaining tests
+    inmemoryDB.put("A", 5); 
 
     // starts a new transaction
     inmemoryDB.begin_transaction();
@@ -18,7 +18,7 @@ public static void main(String[] args) {
     inmemoryDB.put("A", 5);
 
     // should return null, because updates to A are not committed yet
-    System.out.println("2 " + inmemoryDB.get("A")); // return 5
+    System.out.println(inmemoryDB.get("A")); 
 
     // update A’s value to 6 within the transaction
     inmemoryDB.put("A", 6);
@@ -27,16 +27,16 @@ public static void main(String[] args) {
     inmemoryDB.commit();
 
     // should return 6, that was the last value of A to be committed
-    System.out.println("3 " + inmemoryDB.get("A"));
+    System.out.println(inmemoryDB.get("A"));
 
     // throws an error, because there is no open transaction
-    inmemoryDB.commit(); // comment out to test remaining tests
+    inmemoryDB.commit(); 
 
     // throws an error because there is no ongoing transaction
-    inmemoryDB.rollback(); // comment out to test remaining tests
+    inmemoryDB.rollback(); 
 
     // should return null because B does not exist in the database
-    System.out.println("4 " + inmemoryDB.get("B"));
+    System.out.println(inmemoryDB.get("B"));
 
     // starts a new transaction
     inmemoryDB.begin_transaction();
@@ -48,6 +48,6 @@ public static void main(String[] args) {
     inmemoryDB.rollback();
 
     // Should return null because changes to B were rolled back
-    System.out.println("5 " +inmemoryDB.get("B"));
+    System.out.println(inmemoryDB.get("B"));
 }
 }
